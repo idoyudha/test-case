f10d919c2d46e9befa749d3767e2d01d487aab71
idoyudha <idowidya.yudhatama@gmail.com>
2021-07-06 14:33:14 +0700

    update function on case 2, error if date above monthdata and checking month string format

--- case.go
+++ case.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	fmt.Println(sumArray([]int{1, 2, 3}))
-	fmt.Println(dateFormatConversion("Tue 16 Jul 2019"))
+	fmt.Println(dateFormatConversion("Tue 1 Jan 2019"))
 	fmt.Println(calculateFactorial(0))
-	fmt.Println(timeConvert(1,40))
-	fmt.Println(minimumDeletion([]int{3,3,2,1,3}))
+	fmt.Println(timeConvert(1, 40))
+	fmt.Println(minimumDeletion([]int{3, 3, 2, 1, 3}))
 }
 
 // Case 1
@@ -28,13 +28,36 @@ func dateFormatConversion(date string) string {
 	var word = strings.Split(date, " ")
 	var monthNumber = 1
 	var monthsName = [...]string{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+	var monthsDate = [...]int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+
+	// Check month string format
+	if notContainMonth(monthsName[:], word[2]) {
+		return "Please provide the right month format, like ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']"
+	}
+
 	for i := 0; i < len(monthsName); i++ {
 		if monthsName[i] == word[2] {
 			monthNumber += i
 		}
 	}
+
+	// Check if date above the monthsDate array
+	dateNumberString, _ := strconv.Atoi(word[1])
+	if dateNumberString > monthsDate[monthNumber-1] || dateNumberString < 1 {
+		return "Please provide the rigth date number"
+	}
+
 	monthNumberString := strconv.Itoa(monthNumber)
 	return word[3] + "-" + monthNumberString + "-" + word[1]
+}
+
+func notContainMonth(array []string, str string) bool {
+	for _, month := range array {
+		if month == str {
+			return false
+		}
+	}
+	return true
 }
 
 // Case 3
